ff48a02f1f9079df9399281cc3b2d1230401e9c0
Yashar kavosian <kavoosian@hotmail.com>
2020-04-16 22:09:01 +0430

    The application is completed

---
+++ Serial_port_Control/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Serial_port_Control
+{
+    partial class frmSerial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmboPorts = new System.Windows.Forms.ComboBox();
+            this.btnConncect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ports:";
+            // 
+            // cmboPorts
+            // 
+            this.cmboPorts.FormattingEnabled = true;
+            this.cmboPorts.Location = new System.Drawing.Point(80, 5);
+            this.cmboPorts.Name = "cmboPorts";
+            this.cmboPorts.Size = new System.Drawing.Size(196, 34);
+            this.cmboPorts.TabIndex = 1;
+            // 
+            // btnConncect
+            // 
+            this.btnConncect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConncect.Location = new System.Drawing.Point(289, 5);
+            this.btnConncect.Name = "btnConncect";
+            this.btnConncect.Size = new System.Drawing.Size(179, 34);
+            this.btnConncect.TabIndex = 2;
+            this.btnConncect.Text = "&Connect";
+            this.btnConncect.UseVisualStyleBackColor = true;
+            this.btnConncect.Click += new System.EventHandler(this.btnConncect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDisconnect.Location = new System.Drawing.Point(289, 57);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(179, 36);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "&Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 110);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(451, 127);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(17, 252);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(259, 33);
+            this.txtSend.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Location = new System.Drawing.Point(289, 252);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(179, 34);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "&Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // frmSerial
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(480, 457);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConncect);
+            this.Controls.Add(this.cmboPorts);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmSerial";
+            this.Text = "Data Sender Reciever";
+            this.Load += new System.EventHandler(this.frmSerial_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmboPorts;
+        private System.Windows.Forms.Button btnConncect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Serial_port_Control/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serial_port_Control
+{
+    public partial class frmSerial : Form
+    {
+        public delegate void myDelegate();
+        public frmSerial()
+        {
+            InitializeComponent();
+        }//frmSerial
+        private void DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e) {
+            richTextBox1.BeginInvoke(new myDelegate(updateTextBox));
+        }//DataReceived
+        private void frmSerial_Load(object sender, EventArgs e)
+        {
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+            string[] portNames = System.IO.Ports.SerialPort.GetPortNames();
+            cmboPorts.Items.AddRange(portNames);
+            btnDisconnect.Enabled = false;
+        }//frmSerial_Load
+        public void updateTextBox() {
+            richTextBox1.AppendText(serialPort1.ReadExisting());
+            richTextBox1.ScrollToCaret();
+        }//updateTextBox
+
+        private void btnConncect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) {
+                serialPort1.Close();
+            }//if it is open
+            try
+            {
+                serialPort1.PortName = cmboPorts.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Parity = System.IO.Ports.Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                serialPort1.Open();
+                richTextBox1.Text = cmboPorts.Text + "Connected";
+                btnConncect.Enabled = false;
+                btnDisconnect.Enabled = true;
+            }//try
+            catch (Exception ex)
+            {
+                string strmessage, strtitle;
+                strtitle = "Sender & Receiver";
+                strmessage = "An exception with the following message has occured \n\t" + ex.Message;
+                MessageBox.Show(strmessage, strtitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }//catch
+        }//btnConncect_Click
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                richTextBox1.Text = serialPort1.PortName + " disconnected";
+                btnConncect.Enabled = true;
+                btnDisconnect.Enabled = false;           
+            }//try
+            catch (Exception ex)
+            {
+                string strmessage, strtitle;
+                strtitle = "Sender & Receiver";
+                strmessage = "An exception with the following message has occured \n\t" + ex.Message;
+                MessageBox.Show(strmessage, strtitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }//catch
+        }//btnDisconnect_Click
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write(txtSend.Text + Environment.NewLine);
+                richTextBox1.AppendText(">" + txtSend.Text + Environment.NewLine);
+                richTextBox1.ScrollToCaret();
+                txtSend.Clear();
+            }//try
+            catch (Exception ex)
+            {
+                string strmessage, strtitle;
+                strtitle = "Sender & Receiver";
+                strmessage = "An exception with the following message has occured \n\t" + ex.Message;
+                MessageBox.Show(strmessage, strtitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }//catch
+        }//btnSend_Click
+    }//public partial class frmSerial : Form
+}//Serial_port_Control
